75dd350c27d2ba8705c59fd287b6c3091e9f3ac1
tiffany clark <tiffanymarie9811@gmail.com>
2025-03-08 14:36:29 -0500

    updated git ignore

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-import MainMenu from "./db/mainMenu.js";
-const mainMenu = new MainMenu();
-mainMenu.start();
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from "dotenv";
-import chalk from "chalk";
-dotenv.config();
-import MainMenu from "./db/mainMenu.js";
-const mainMenu = new MainMenu();
-mainMenu.start();
